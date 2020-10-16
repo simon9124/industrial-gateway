fe34aee480bb0d7d6311026f5424128908e0d106
simon9124 <435776313@qq.com>
2020-10-16 20:38:24 +0800

    模拟数据更新
    Signed-off-by: simon9124 <435776313@qq.com>

--- src/mock/content.js
+++ src/mock/content.js
@@ -9,7 +9,27 @@ const passList = [
       describe: "MODBUS TCP",
       factory: "莫迪康",
       classification: "通用标准",
-      path: "C:\\Users\\43577\\Desktop\\软件\\CESTC\\PluginIo\\IND_MODBUS_RTU"
+      path: "C:\\Users\\43577\\Desktop\\软件\\CESTC\\PluginIo\\IND_MODBUS_RTU",
+      plugins: [
+        {
+          name: "BA_BACNET_IP.335x",
+          platform: "335x",
+          edition: "5.0.0.1",
+          lastModifiedDate: "2017-11-08 07:55:30"
+        },
+        {
+          name: "BA_BACNET_IP.dll",
+          platform: "dll",
+          edition: "5.0.0.1",
+          lastModifiedDate: "2019-02-19 03:45:30"
+        },
+        {
+          name: "BA_BACNET_IP.xt",
+          platform: "xt",
+          edition: "",
+          lastModifiedDate: "2020-02-22 08:47:32"
+        }
+      ]
     },
     passType: "串口",
     sata: "COM01",
@@ -49,7 +69,27 @@ const passList = [
       describe: "T188 2004",
       factory: "城建",
       classification: "通用标准",
-      path: "C:\\Users\\43577\\Desktop\\软件\\CESTC\\PluginIo\\IND_CJT188_2004"
+      path: "C:\\Users\\43577\\Desktop\\软件\\CESTC\\PluginIo\\IND_CJT188_2004",
+      plugins: [
+        {
+          name: "IND_CJT188_2004.335x",
+          platform: "335x",
+          edition: "5.0.0.1",
+          lastModifiedDate: "2017-11-08 07:55:30"
+        },
+        {
+          name: "IND_CJT188_2004.dll",
+          platform: "dll",
+          edition: "5.0.0.1",
+          lastModifiedDate: "2019-02-19 03:45:30"
+        },
+        {
+          name: "IND_CJT188_2004.xt",
+          platform: "xt",
+          edition: "",
+          lastModifiedDate: "2020-02-22 08:47:32"
+        }
+      ]
     },
     passType: "虚拟端口",
     sata: null,
@@ -89,7 +129,28 @@ const passList = [
       describe: "AB LOGIX 5000 TCP",
       factory: "AB",
       classification: "PLC",
-      path: "C:\\Users\\43577\\Desktop\\软件\\CESTC\\PluginIo\\PLC_AB_LOGIX_TCP"
+      path:
+        "C:\\Users\\43577\\Desktop\\软件\\CESTC\\PluginIo\\PLC_AB_LOGIX_TCP",
+      plugins: [
+        {
+          name: "PLC_AB_LOGIX_TCP.335x",
+          platform: "335x",
+          edition: "5.0.0.1",
+          lastModifiedDate: "2017-11-08 07:55:30"
+        },
+        {
+          name: "PLC_AB_LOGIX_TCP.dll",
+          platform: "dll",
+          edition: "5.0.0.1",
+          lastModifiedDate: "2019-02-19 03:45:30"
+        },
+        {
+          name: "PLC_AB_LOGIX_TCP.xt",
+          platform: "xt",
+          edition: "",
+          lastModifiedDate: "2020-02-22 08:47:32"
+        }
+      ]
     },
     passType: "串口",
     sata: "COM01",
--- src/mock/plugin.js
+++ src/mock/plugin.js
@@ -14,7 +14,28 @@ const pluginList = [
         describe: "MODBUS TCP",
         factory: "莫迪康",
         classification: "通用标准",
-        path: "C:\\Users\\43577\\Desktop\\软件\\CESTC\\PluginIo\\IND_MODBUS_RTU"
+        path:
+          "C:\\Users\\43577\\Desktop\\软件\\CESTC\\PluginIo\\IND_MODBUS_RTU",
+        plugins: [
+          {
+            name: "BA_BACNET_IP.335x",
+            platform: "335x",
+            edition: "5.0.0.1",
+            lastModifiedDate: "2017-11-08 07:55:30"
+          },
+          {
+            name: "BA_BACNET_IP.dll",
+            platform: "dll",
+            edition: "5.0.0.1",
+            lastModifiedDate: "2019-02-19 03:45:30"
+          },
+          {
+            name: "BA_BACNET_IP.xt",
+            platform: "xt",
+            edition: "",
+            lastModifiedDate: "2020-02-22 08:47:32"
+          }
+        ]
       },
       {
         text: "T188 2004",
@@ -26,7 +47,27 @@ const pluginList = [
         factory: "城建",
         classification: "通用标准",
         path:
-          "C:\\Users\\43577\\Desktop\\软件\\CESTC\\PluginIo\\IND_CJT188_2004"
+          "C:\\Users\\43577\\Desktop\\软件\\CESTC\\PluginIo\\IND_CJT188_2004",
+        plugins: [
+          {
+            name: "IND_CJT188_2004.335x",
+            platform: "335x",
+            edition: "5.0.0.1",
+            lastModifiedDate: "2017-11-08 07:55:30"
+          },
+          {
+            name: "IND_CJT188_2004.dll",
+            platform: "dll",
+            edition: "5.0.0.1",
+            lastModifiedDate: "2019-02-19 03:45:30"
+          },
+          {
+            name: "IND_CJT188_2004.xt",
+            platform: "xt",
+            edition: "",
+            lastModifiedDate: "2020-02-22 08:47:32"
+          }
+        ]
       }
     ]
   },
@@ -45,7 +86,15 @@ const pluginList = [
         describe: "DNP3.0协议",
         factory: "",
         classification: "电力行业标准",
-        path: "C:\\Users\\43577\\Desktop\\软件\\CESTC\\PluginIo\\DL_DNP3"
+        path: "C:\\Users\\43577\\Desktop\\软件\\CESTC\\PluginIo\\DL_DNP3",
+        plugins: [
+          {
+            name: "DL_DNP3.dll",
+            platform: "dll",
+            edition: "5.0.0.1",
+            lastModifiedDate: "2019-02-19 03:45:30"
+          }
+        ]
       },
       {
         text: "376.1主站",
@@ -57,7 +106,27 @@ const pluginList = [
         factory: "电力规约",
         classification: "电力行业标准",
         path:
-          "C:\\Users\\43577\\Desktop\\软件\\CESTC\\PluginIo\\ELE_376_1_MASTER"
+          "C:\\Users\\43577\\Desktop\\软件\\CESTC\\PluginIo\\ELE_376_1_MASTER",
+        plugins: [
+          {
+            name: "ELE_376_1_MASTER.335x",
+            platform: "335x",
+            edition: "5.0.0.1",
+            lastModifiedDate: "2017-11-08 07:55:30"
+          },
+          {
+            name: "ELE_376_1_MASTER.dll",
+            platform: "dll",
+            edition: "5.0.0.1",
+            lastModifiedDate: "2019-02-19 03:45:30"
+          },
+          {
+            name: "ELE_376_1_MASTER.xt",
+            platform: "xt",
+            edition: "",
+            lastModifiedDate: "2020-02-22 08:47:32"
+          }
+        ]
       }
     ]
   },
@@ -76,7 +145,27 @@ const pluginList = [
         describe: "AB DF1串口协议",
         factory: "AB(罗克韦尔)",
         classification: "PLC",
-        path: "C:\\Users\\43577\\Desktop\\软件\\CESTC\\PluginIo\\PLC_AB_DF1"
+        path: "C:\\Users\\43577\\Desktop\\软件\\CESTC\\PluginIo\\PLC_AB_DF1",
+        plugins: [
+          {
+            name: "PLC_AB_DF1.335x",
+            platform: "335x",
+            edition: "5.0.0.1",
+            lastModifiedDate: "2017-11-08 07:55:30"
+          },
+          {
+            name: "PLC_AB_DF1.dll",
+            platform: "dll",
+            edition: "5.0.0.1",
+            lastModifiedDate: "2019-02-19 03:45:30"
+          },
+          {
+            name: "PLC_AB_DF1.xt",
+            platform: "xt",
+            edition: "",
+            lastModifiedDate: "2020-02-22 08:47:32"
+          }
+        ]
       },
       {
         text: "AB LOGIX 5000 TCP",
@@ -88,7 +177,27 @@ const pluginList = [
         factory: "AB",
         classification: "PLC",
         path:
-          "C:\\Users\\43577\\Desktop\\软件\\CESTC\\PluginIo\\PLC_AB_LOGIX_TCP"
+          "C:\\Users\\43577\\Desktop\\软件\\CESTC\\PluginIo\\PLC_AB_LOGIX_TCP",
+        plugins: [
+          {
+            name: "PLC_AB_LOGIX_TCP.335x",
+            platform: "335x",
+            edition: "5.0.0.1",
+            lastModifiedDate: "2017-11-08 07:55:30"
+          },
+          {
+            name: "PLC_AB_LOGIX_TCP.dll",
+            platform: "dll",
+            edition: "5.0.0.1",
+            lastModifiedDate: "2019-02-19 03:45:30"
+          },
+          {
+            name: "PLC_AB_LOGIX_TCP.xt",
+            platform: "xt",
+            edition: "",
+            lastModifiedDate: "2020-02-22 08:47:32"
+          }
+        ]
       }
     ]
   }
