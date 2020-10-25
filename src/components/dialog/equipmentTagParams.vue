a69390352cfa0df5e500b2b9b686590c59390dbd
simon9124 <435776313@qq.com>
2020-10-25 22:32:09 +0800

    临时
    Signed-off-by: simon9124 <435776313@qq.com>

--- src/components/dialog/equipmentTagParams.vue
+++ src/components/dialog/equipmentTagParams.vue
@@ -2,23 +2,44 @@
   <el-dialog :title="dialogTitle"
              :visible.sync="dialogVisible">
 
-    <!-- <el-checkbox v-model="formData.ratioCalculation"
-                 style="margin-right:20px">系数计算</el-checkbox>
-    <div class="params-dialog-row-div">
-      &nbsp;倍率（a）
-      <el-input v-model="formData.magnification"
-                style="width:100px"
-                :disabled="!formData.ratioCalculation"></el-input>
-      &nbsp;基数（b）
-      <el-input v-model="formData.base"
-                style="width:100px;margin-right:20px"
-                :disabled="!formData.ratioCalculation"></el-input>
-      形如：y = ax + b
-    </div> -->
+    <el-row class="params-dialog-row">
+      <el-checkbox v-model="formData.otherParams.takeAbsoluteValue"
+                   style="margin-right:20px">取绝对值</el-checkbox>
+      &nbsp;单位&nbsp;
+      <el-input v-model="formData.otherParams.absoluteValue"
+                style="width:100px;margin-right:20px"></el-input>
+      &nbsp;设备系数计算&nbsp;
+      <el-select v-model="formData.otherParams.calculation"
+                 placeholder="请选择"
+                 style="width:200px;margin-right:20px">
+        <el-option v-for="item in calculationList"
+                   :key="item"
+                   :label="item"
+                   :value="item">
+        </el-option>
+      </el-select>
+    </el-row>
+
+    <el-row class="params-dialog-row">
+      <el-checkbox v-model="formData.otherParams.ratioCalculation"
+                   style="margin-right:20px">系数计算</el-checkbox>
+      <div class="params-dialog-row-div">
+        &nbsp;倍率（a）
+        <el-input v-model="formData.otherParams.magnification"
+                  style="width:100px"
+                  :disabled="!formData.otherParams.ratioCalculation"></el-input>
+        &nbsp;基数（b）
+        <el-input v-model="formData.otherParams.base"
+                  style="width:100px;margin-right:20px"
+                  :disabled="!formData.otherParams.ratioCalculation"></el-input>
+        形如：y = ax + b
+      </div>
+    </el-row>
 
     <div slot="footer"
          class="dialog-footer">
-      <el-button @click="dialogVisible = false;formData.otherParams = paramsOrg">取 消</el-button>
+      <el-button @click="dialogVisible = false;formData.otherParams = paramsOrg;">
+        取 消</el-button>
       <el-button @click="dialogVisible = false"
                  type="primary">确 定</el-button>
     </div>
@@ -26,7 +47,6 @@
 </template>
 
 <script>
-
 export default {
   props: {
     // 当前标签数据
@@ -37,7 +57,8 @@ export default {
   data () {
     return {
       dialogVisible: false, // 是否可见
-      dialogTitle: "" // 弹框名称
+      dialogTitle: "", // 弹框名称
+      calculationList: ["不参与计算", "乘设备系数R1", "乘设备系数R2", "乘设备系数R1*R2"] // 设备系数列表
     };
   },
   methods: {
@@ -52,5 +73,4 @@ export default {
 };
 </script>
 
-<style rel="stylesheet/scss" lang="scss" scoped>
-</style>
+<style rel="stylesheet/scss" lang="scss" scoped></style>
