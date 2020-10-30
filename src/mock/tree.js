97eae9762eaf2a461c5f615cba047645899e4778
simon9124 <435776313@qq.com>
2020-10-30 16:40:45 +0800

    模拟数据更新
    Signed-off-by: simon9124 <435776313@qq.com>

--- src/mock/tree.js
+++ src/mock/tree.js
@@ -1,4 +1,4 @@
-// 树数据
+// 树数据 - 完整
 const treeData = [
   {
     text: "采集服务",
@@ -65,4 +65,67 @@ const treeData = [
   }
 ];
 
-export { treeData };
+// 树数据 - 模板
+const treeTempleteData = [
+  {
+    text: "采集服务",
+    icon: "fa fa-folder",
+    id: "1",
+    level: 1,
+    selected: true,
+    opened: true, // 父节点须设置opened为true，否则子节点首次新增时打不开
+    children: []
+  },
+  {
+    text: "数据服务",
+    icon: "fa fa-database",
+    id: "2",
+    level: 1,
+    opened: true,
+    children: []
+  }
+];
+
+// 工程数据
+const factoryData = [
+  {
+    text: "工程组列表",
+    icon: "fa fa-folder",
+    id: "root",
+    level: 1,
+    children: [
+      {
+        text: "工程组1",
+        icon: "fa fa-laptop",
+        id: "group-1",
+        level: 2,
+        children: [
+          {
+            text: "采集器工程1",
+            icon: "fa fa-edit",
+            id: "factory-1",
+            selected: true,
+            level: 3,
+            treeData: treeData
+          },
+          {
+            text: "采集器工程2",
+            icon: "fa fa-edit",
+            id: "factory-2",
+            level: 3,
+            treeData: treeTempleteData
+          }
+        ]
+      },
+      {
+        text: "工程组2",
+        icon: "fa fa-laptop",
+        id: "group-2",
+        level: 2,
+        children: []
+      }
+    ]
+  }
+];
+
+export { factoryData };
