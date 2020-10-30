4db64daa31ce5920f4049ca3a6ed78886e662d02
simon9124 <435776313@qq.com>
2020-10-30 11:04:00 +0800

    eslint规则更新：不检查函数后空格问题
    Signed-off-by: simon9124 <435776313@qq.com>

--- .eslintrc.js
+++ .eslintrc.js
@@ -24,6 +24,7 @@ module.exports = {
     // allow debugger during development
     "no-debugger": process.env.NODE_ENV === "production" ? "error" : "off",
     quotes: [1, "double"],
-    semi: ["error", "always"]
+    semi: ["error", "always"],
+    "space-before-function-paren": "off"
   }
 };
