4baf56c56c5af894670eb6f7b940856effbcee9f
simon9124 <435776313@qq.com>
2020-11-03 17:53:43 +0800

    新增工程组和工程
    Signed-off-by: simon9124 <435776313@qq.com>

--- src/components/dialog/factoryManage.vue
+++ src/components/dialog/factoryManage.vue
@@ -60,21 +60,33 @@
       <!-- 采集工程 -->
       <el-form v-if="level===3||(level===2&&handleType==='insert')"
                ref="formFactory"
-               :model="formFactory">
-        formFactory
-
+               :model="formFactory"
+               label-width="90px"
+               label-position="right">
+        <el-form-item prop="text"
+                      label="工程名：">
+          <el-input v-model="formFactory.text"></el-input>
+        </el-form-item>
+        <el-form-item prop="describe"
+                      label="工程描述：">
+          <el-input v-model="formFactory.describe"></el-input>
+        </el-form-item>
       </el-form>
 
       <div slot="footer"
            class="dialog-footer">
         <el-button @click="dialogManageVisible = false">取 消</el-button>
-        <el-button type="primary">确 定</el-button>
+        <el-button @click="submit"
+                   type="primary"
+                   :loading="submitLoading">确 定</el-button>
       </div>
     </el-dialog>
   </div>
 </template>
 
 <script>
+import { parseTime } from "@/utils"; // function - 格式化时间
+import { treeTempleteData } from "@/mock/tree";
 import LeftTree from "@/components/Tree"; // 组件：左侧树
 
 export default {
@@ -97,8 +109,9 @@ export default {
       dialogManageTitle: "", // 新增/修改 - 弹框名称
       /* tree */
       level: 3, // 被选择的树的层级
-      id: null, // 被选择内容的id
-      handleType: "", // 树节点的操作方式
+      id: null, // 被选择的树的id
+      dialogType: "", // dialog的种类 group/factory
+      handleType: "", // dialog的提交方式 insert/edit
       /* 工程组 */
       formGroup: {
         text: "",
@@ -107,21 +120,28 @@ export default {
       }, // 表单数据
       formGroupOrg: {}, // 表单数据 - 原始
       /* 工程 */
-      formFactory: {}, // 表单数据
-      formFactoryOrg: {} // 表单数据 - 原始
-
+      formFactory: { // 表单数据
+        text: "",
+        describe: ""
+      },
+      formFactoryOrg: {}, // 表单数据 - 原始
+      /* loading */
+      submitLoading: false
     };
   },
   methods: {
     // 工程管理
     factoryManage () {
       this.dialogVisible = true;
-      // this.paramsOrg = JSON.parse(JSON.stringify(this.formPass.otherParams)); // 深拷贝，取消时还原数据用
     },
+    // 获取被选择的工程
+    getSelectedFactory () { },
     // 点击树节点
     itemClick (param) {
-      console.log(param);
-      this.level = param.level;
+      // console.log(param);
+      const { id, level } = param;
+      this.id = id;
+      this.level = level;
       this.level === 2 && (this.formGroupOrg = param);
       this.level === 3 && (this.formFactoryOrg = param);
       this.$emit("factory-select", param);
@@ -131,9 +151,11 @@ export default {
       this.dialogManageVisible = true;
       this.handleType = "insert";
       this.dialogManageTitle = `新建${this.level === 1 ? "项目组" : "采集工程"}`;
+      this.dialogType = this.level === 1 ? "group" : "factory";
       this.$nextTick(() => {
         this.level === 1 && this.$refs.formGroup.resetFields();
         this.level === 2 && this.$refs.formFactory.resetFields();
+        this.formGroup.creatTime = parseTime(new Date());
       });
     },
     // 修改
@@ -141,10 +163,56 @@ export default {
       this.dialogManageVisible = true;
       this.handleType = "edit";
       this.dialogManageTitle = `修改${this.level === 2 ? "项目组" : "采集工程"}`;
+      this.dialogType = this.level === 2 ? "group" : "factory";
       this.$nextTick(() => {
         this.level === 2 && (this.formGroup = JSON.parse(JSON.stringify(this.formGroupOrg)));
         this.level === 3 && (this.formFactory = JSON.parse(JSON.stringify(this.formFactoryOrg)));
       });
+    },
+    // 表单提交：增/改
+    submit () {
+      const formData = this.dialogType === "group"
+        ? JSON.parse(JSON.stringify(this.formGroup)) : JSON.parse(JSON.stringify(this.formFactory));
+      this.submitLoading = true;
+      // console.log(formData);
+      switch (this.handleType) {
+        case "insert":
+          /* 1.完善数据 */
+          formData.id = Math.random().toString(36).substr(-10);// 随机生成id
+          formData.icon = this.dialogType === "group" ? "fa fa-laptop" : "fa fa-edit";
+          formData.level = this.dialogType === "group" ? 2 : 3;
+          formData.selected = false; // 未被选中的父节点需设置selected为false，否则该节点切换时会有bug
+          formData.opened = true; // 父节点须设置opened为true，否则子节点首次新增时打不开
+          /* 2.塞入数据 */
+          if (this.dialogType === "group") { // 新增工程组
+            formData.children = [];
+            this.factoryData[0].children.push(formData);
+          } else { // 新增工程
+            formData.treeData = JSON.parse(JSON.stringify(treeTempleteData));
+            this.factoryData[0].children.forEach((group, i) => {
+              this.id === group.id && group.children.push(formData);
+            });
+          }
+          /* 3.重新选中当前的工程 */
+
+          this.submitLoading = false;
+          this.dialogManageVisible = false;
+          break;
+        case "edit":
+          this.factoryData[0].children.forEach((group, i) => {
+            // 工程组修改
+            this.dialogType === "group" && formData.id === group.id &&
+              this.$set(this.factoryData[0].children, i, JSON.parse(JSON.stringify(formData)));
+            // 工程修改
+            this.dialogType === "factory" && group.children.forEach((factory, _i) => {
+              formData.id === factory.id &&
+                this.$set(group.children, _i, JSON.parse(JSON.stringify(formData)));
+            });
+          });
+          this.submitLoading = false;
+          this.dialogManageVisible = false;
+          break;
+      }
     },
     // 删除
     itemDelete () { }
