808c3f0a8b560db9bf0b2857699b8ee59d97c628
simon9124 <435776313@qq.com>
2020-11-20 18:26:44 +0800

    远程监视：监视数据框 - 左侧树和表格的数据处理
    Signed-off-by: simon9124 <435776313@qq.com>

--- src/components/dialog/remoteMonitor.vue
+++ src/components/dialog/remoteMonitor.vue
@@ -166,6 +166,7 @@ export default {
       dialogTitleMonitor: "", // 弹框名称 - 监视左数右表
       monitorTree: [], // 监视数据 - 左侧树
       monitorTable: [], // 监视数据 - 右侧表格
+      handle: 0, // 需要递增的handle列项
       /* 动态高度 */
       tableMaxHeight: 0 // 流体表格最大高度
     };
@@ -177,8 +178,6 @@ export default {
     // 渲染左侧树
     getTreeData () {
       const treeData = JSON.parse(JSON.stringify(this.treeData)); // 深拷贝
-      treeData[0].selected = false; // 取消选择“采集服务”
-      console.log(this.treeData);
       this.monitorTree = [
         {
           text: "系统",
@@ -189,7 +188,8 @@ export default {
           opened: true,
           children: treeData
         }
-      ]; // 渲染左侧树
+      ];
+      this.handleRecursion(this.monitorTree); // 左侧树数据递归处理
     },
     // ip选择
     ipSelect () {
@@ -198,11 +198,13 @@ export default {
     // 弹出监视框
     remoteMonitor (row) {
       // console.log(row);
+      this.handle = 0;
       this.dialogTitleMonitor = row.address;
       this.dialogVisibleMonitor = true;
       this.getTableMaxHeight(); // 表格最大高度
-      this.monitorTable = systemDic; // 渲染表格
-      this.refreshData(); // 表格数据处理
+      this.handleRecursion(this.monitorTree); // 左侧树数据递归处理
+      this.monitorTable = this.monitorTree[0].tableData; // 渲染表格
+      this.refreshTableData(); // 表格数据处理
     },
     // 表格最大高度
     getTableMaxHeight () {
@@ -212,26 +214,43 @@ export default {
     // 点击左侧树节点
     itemClick (param) {
       console.log(param);
-      param.level === 0 && (this.monitorTable = systemDic);
-      param.level === 1 && (this.monitorTable = []);
-      if (param.level === 2) {
-        param.id.slice(0, 1) === "1" &&
-          (this.monitorTable = passDic.filter(pass => pass.type.indexOf("collect") > -1));
-        param.id.slice(0, 1) === "2" &&
-          (this.monitorTable = passDic.filter(pass => pass.type.indexOf("data") > -1));
-      }
-      if (param.level === 3) {
-        this.monitorTable = equipmentDic;
-      }
-      this.refreshData();
+      this.monitorTable = param.tableData || [];
+      this.refreshTableData();
+    },
+    // 左侧树数据处理 - 递归函数
+    handleRecursion (recursionData) {
+      recursionData.forEach(item => {
+        const params = {
+          opened: true,
+          selected: item.level === 0,
+          tableData:
+            item.level === 0 ? JSON.parse(JSON.stringify(systemDic))
+              : item.level === 1 ? []
+                : item.level === 3 ? JSON.parse(JSON.stringify(equipmentDic))
+                  : item.id.slice(0, 1) === "1"
+                    ? JSON.parse(JSON.stringify(passDic.filter(pass => pass.type.indexOf("collect") > -1)))
+                    : JSON.parse(JSON.stringify(passDic.filter(pass => pass.type.indexOf("data") > -1)))
+        };
+        this.dataPackage(item, params);
+        item.children && this.handleRecursion(item.children);
+      });
+    },
+    // 封装：左侧树每层数据处理
+    dataPackage (item, params) {
+      Object.keys(params).forEach(key => {
+        this.$set(item, key, params[key]); // 遍历对象：①选中与否 ②展开 ③设置tableData
+      });
+      item.tableData.forEach(_item => { // handle列项
+        this.$set(_item, "handle", this.handle);
+        this.handle++;
+      });
     },
     // 表格数据处理
-    refreshData () {
+    refreshTableData () {
       this.monitorTable.forEach((item, i) => {
         this.$set(item, "curTime", parseTime(new Date()));
         this.$set(item, "qualityStamp", "good");
-        this.$set(item, "changeTimes", "1");
-        this.$set(item, "handle", i);
+        this.$set(item, "changeTimes", Math.floor(Math.random() * (1 - 101) + 101));
         this.$set(item, "direction", "只读");
         item.name === "_project_name" && this.factoryData[0].children.forEach(group => {
           group.children.forEach(factory => {
